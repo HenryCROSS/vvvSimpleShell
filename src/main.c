084ee44e248567910a9aa44966656278a1a81284
HenryCROSS <henry583050973@gmail.com>
2023-04-29 00:38:11 -0400

    setup and add lsh_read_line()

---
+++ src/main.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char *lsh_read_line(void)
+{
+    char *line = NULL;
+    ssize_t bufsize = 0; // have getline allocate a buffer for us
+
+    // getline will allocate memory!
+    if (getline(&line, &bufsize, stdin) == -1)
+    {
+        if (feof(stdin))
+        {
+            exit(EXIT_SUCCESS); // We recieved an EOF
+        }
+        else
+        {
+            perror("readline");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    return line;
+}
+
+void lsh_loop(void)
+{
+    char *line;
+    char **args;
+    int status;
+
+    do
+    {
+        printf("> ");
+        line = lsh_read_line();
+        args = lsh_split_line(line);
+        status = lsh_execute(args);
+
+        free(line);
+        free(args);
+    } while (status);
+
+    int main(int argc, char *argv[])
+    {
+        // Load config files, if any
+        // Run command loop.
+        lsh_loop();
+
+        // Perform any shutdown/cleanup
+        return 0;
+    }
